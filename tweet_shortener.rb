fd2ed29d2448cdfe46801ef0d2c49788a6320d61
Xinran Che <gracexinran@gmail.com>
2019-05-21 22:14:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,7 @@ def word_substituter(tweet)
   
   def  shortened_tweet_truncator(tweet)
     if tweet.length > 140 
-      word_substituter(tweet)[0..140]
+      word_substituter(tweet)[0..139]
     else
       tweet 
     end
