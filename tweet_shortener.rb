da5dec5c06f3826b9ebca2b192907aa7cd7bb24b
Xinran Che <gracexinran@gmail.com>
2019-05-21 21:52:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def word_substituter(tweet)
+  dictionary = {
+    "hi" => ["hello"],
+    '2' => ["to", 'two', 'too'],
+    '4' => ['for', 'four'],
+    'b' => ['be'],
+    'u' => ['you'],
+    '@' => ['at'],
+    '&' => ['and']
+  }
+    tweets = tweet.split 
+    dictionary.each do |k, v|
+      v.each do |word|
+        tweets.each do |i|
+          if i == word 
+            i = k
+          end 
+        end
+      end
+    end
+    tweets.join(' ')
+  end
+          
+    
